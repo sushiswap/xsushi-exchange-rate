5ce81d8c2608121b5ac1538b70da22dc7d16fc0d
Keno <kenobudde@gmail.com>
2021-02-09 11:11:49 +0100

    Changed compiler version to 0.6.12 replaced IERC20 interface with import linted file

--- contracts/xSushiExchangeRate.sol
+++ contracts/xSushiExchangeRate.sol
@@ -1,29 +1,25 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface IERC20 {
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address account) external view returns (uint256);
-}
+pragma solidity 0.6.12;
+import "@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol";
 
 contract xSushiExchangeRate {
-  IERC20 private immutable xSushi;
-  IERC20 private immutable sushi;
+    IERC20 private immutable xSushi;
+    IERC20 private immutable sushi;
 
-  constructor(address _xSushi, address _sushi) {
-    xSushi = IERC20(_xSushi);
-    sushi = IERC20(_sushi);
-  }
+    constructor(IERC20 _xSushi, IERC20 _sushi) public {
+        xSushi = _xSushi;
+        sushi = _sushi;
+    }
 
-  function getExchangeRate() public view returns( uint256 ) {
-    return sushi.balanceOf(address(xSushi))*(1e18) / xSushi.totalSupply();
-  }
-  
-  function toSUSHI(uint256 xSushiAmount) public view returns (uint256 sushiAmount) {
-    sushiAmount = xSushiAmount * sushi.balanceOf(address(xSushi)) / xSushi.totalSupply();
-  }
-  
-  function toXSUSHI(uint256 sushiAmount) public view returns (uint256 xSushiAmount) {
-    xSushiAmount = sushiAmount * xSushi.totalSupply() / sushi.balanceOf(address(xSushi));
-  }
+    function getExchangeRate() public view returns (uint256) {
+        return (sushi.balanceOf(address(xSushi)) * 1e18) / xSushi.totalSupply();
+    }
+
+    function toSUSHI(uint256 xSushiAmount) public view returns (uint256 sushiAmount) {
+        sushiAmount = (xSushiAmount * sushi.balanceOf(address(xSushi))) / xSushi.totalSupply();
+    }
+
+    function toXSUSHI(uint256 sushiAmount) public view returns (uint256 xSushiAmount) {
+        xSushiAmount = (sushiAmount * xSushi.totalSupply()) / sushi.balanceOf(address(xSushi));
+    }
 }
